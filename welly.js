fe7c4b91968cf068eb56f34e9987a69ba89b1bfa
startCoding <abid302084@gmail.com>
2024-11-14 11:50:00 +0600

    add new file

--- welly.js
+++ welly.js
@@ -1,1 +1,28 @@
 let dt = document.querySelector("#start");
+
+
+const product = document.querySelector("#product");
+
+const productDiv = document.querySelector("#product_div");
+
+product.addEventListener("mouseover", function(){
+
+productDiv.style.display = "block";
+
+});
+
+productDiv.addEventListener("mouseout", function(){
+
+    productDiv.style.display = "none";
+    
+    });
+    
+productDiv.addEventListener("mouseover", function(){
+
+    productDiv.style.display = "block";
+    
+    });
+
+    
+
+   
