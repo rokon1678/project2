7c265a238b6647a92fb16915cef5f84fa63d0971
startCoding <abid302084@gmail.com>
2024-11-18 22:06:49 +0600

    add new info

--- welly.js
+++ welly.js
@@ -23,6 +23,15 @@ productDiv.addEventListener("mouseover", function(){
     
     });
 
+
+    const image1 = document.querySelector("#image1");
+    const image2 = document.querySelector("#image2");
+    const hoverText1 = document.querySelector("#hoverText1");
+    const hoverText2 = document.querySelector("#hoverText2");
+
+    hoverText1.addEventListener("mousemove", () => {
+        image1.getAttribute('.product_image_div');
+    });
     
 
    
